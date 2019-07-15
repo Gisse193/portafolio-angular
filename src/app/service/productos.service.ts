a9d3f43bd74cc6610fd5483c359198dcfb76c0de
Karol Gisselle <kgramirez56@misena.edu.co>
2019-07-15 16:54:04 -0500

    Fin seccion 7

--- src/app/service/productos.service.ts
+++ src/app/service/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from 'src/app/interfaces/productos.interface';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -8,20 +9,46 @@ import { Producto } from 'src/app/interfaces/productos.interface';
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productoFiltrado: Producto[] = [];
   constructor( private http: HttpClient ) {
     this.cargarProductos();
    }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-a0301.firebaseio.com/productos_idx.json')
+    return new Promise(( resolve, reject ) => {
+      this.http.get('https://angular-html-a0301.firebaseio.com/productos_idx.json')
         .subscribe( (resp: Producto[]) => {
-
-          console.log(resp);
           this.productos = resp;
           this.cargando = false;
-          /* setTimeout(() => {
-            this.cargando = false;
-          }, 2000); */
+          resolve();
         });
+    });
+}
+getProducto(id: string) {
+  return this.http.get(`https://angular-html-a0301.firebaseio.com/productos/${ id }.json`);
+}
+buscarProducto( termino: string ) {
+  if (this.productoFiltrado.length === 0) {
+    /*cargar productos*/
+    this.cargarProductos().then(() => {
+    this.filtrarProductos( termino );
+    });
+  }else{
+    /*aplicar filtro*/
+    this.filtrarProductos( termino );
+  }
+}
+private filtrarProductos( termino: String ) {
+/*  console.log(this.productos); */
+ this.productoFiltrado = [];
+ termino = termino.toLocaleLowerCase();
+ this.productos.forEach( prod => {
+   const tituloLower = prod.titulo.toLocaleLowerCase();
+   if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+   this.productoFiltrado.push( prod );
+  }
+});
 }
 }
+
+/*setTimeout(() => {this.cargando = false;}, 2000);*/
